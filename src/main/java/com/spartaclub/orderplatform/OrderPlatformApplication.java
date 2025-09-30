4b9e9b7d7fd3eeea090958865b10530a117b6186
전우선 <jeonwooseon@naver.com>
2025-09-30 15:39:49 +0900

    feat: BaseEntity 및 User 엔티티 구현

--- src/main/java/com/spartaclub/orderplatform/OrderPlatformApplication.java
+++ src/main/java/com/spartaclub/orderplatform/OrderPlatformApplication.java
@@ -2,8 +2,10 @@ package com.spartaclub.orderplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // Spring Data JPA Auditing 기능 활성화
 public class OrderPlatformApplication {
 
 	public static void main(String[] args) {
