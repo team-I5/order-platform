e1fe51db8ccc4dd01e124fa3fb2b07e570555cc7
kmr5326 <kmr5326@naver.com>
2025-10-02 12:39:05 +0900

    feat: ãOrder, OrderProduct, Payment 연관관계  추가

--- src/main/java/com/spartaclub/orderplatform/domain/order/domain/model/Order.java
+++ src/main/java/com/spartaclub/orderplatform/domain/order/domain/model/Order.java
@@ -1,6 +1,8 @@
 package com.spartaclub.orderplatform.domain.order.domain.model;
 
 import com.spartaclub.orderplatform.domain.payment.domain.model.Payment;
+import com.spartaclub.orderplatform.domain.store.entity.Store;
+import com.spartaclub.orderplatform.domain.user.entity.User;
 import com.spartaclub.orderplatform.global.entity.BaseEntity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -9,8 +11,13 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -24,7 +31,7 @@ import org.hibernate.annotations.UuidGenerator;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Builder
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Order extends BaseEntity {
 
     @Id
@@ -32,11 +39,19 @@ public class Order extends BaseEntity {
     @Column(name = "orderId", nullable = false, updatable = false, columnDefinition = "uuid")
     private UUID orderId;
 
-    @Column(name = "userId", nullable = false)
-    private Long userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private User userId;
 
-    @Column(name = "storeId", nullable = false, columnDefinition = "uuid")
-    private UUID storeId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store storeId;
+
+    @OneToMany(mappedBy = "order",
+        cascade = CascadeType.ALL,        // Order 저장/삭제 시 자식도 같이
+        orphanRemoval = true,             // 컬렉션에서 제거 시 DB에서도 삭제
+        fetch = FetchType.LAZY)
+    private List<OrderProduct> orderProducts = new ArrayList<>();
 
     @Column(name = "totalPrice", nullable = false)
     private Long totalPrice;              // 총 금액
@@ -55,6 +70,7 @@ public class Order extends BaseEntity {
 
     @Column(name = "address", length = 255)
     private String address;               // 배송 주소
+    // TODO: 주소 엔티티 만들어지면 연관관계 설정하기
 
     @Enumerated(EnumType.STRING)
     @Column(name = "status", nullable = false, length = 40)
--- src/main/java/com/spartaclub/orderplatform/domain/order/domain/model/OrderProduct.java
+++ src/main/java/com/spartaclub/orderplatform/domain/order/domain/model/OrderProduct.java
@@ -1,9 +1,13 @@
 package com.spartaclub.orderplatform.domain.order.domain.model;
 
+import com.spartaclub.orderplatform.domain.product.entity.Product;
 import com.spartaclub.orderplatform.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.util.UUID;
 import lombok.AccessLevel;
@@ -22,30 +26,30 @@ public class OrderProduct extends BaseEntity {
     @Column(name = "orderProductId", nullable = false, updatable = false, columnDefinition = "uuid")
     private UUID orderProductId;
 
-    @Column(name = "orderId", nullable = false, columnDefinition = "uuid")
-    private UUID orderId;                   // 주문 FK
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "order_id", nullable = false, columnDefinition = "uuid")
+    private Order orderId;                   // 주문 FK
 
-    @Column(name = "productId", nullable = false, columnDefinition = "uuid")
-    private UUID productId;                 // 상품 FK
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "product_id", nullable = false, columnDefinition = "uuid")
+    private Product productId;                 // 상품 FK
 
     @Column(name = "quantity", nullable = false)
     private Integer quantity;               // 수량
 
     @Column(name = "unitPrice", nullable = false)
-    private Long unitPrice;           // 주문 시점 단가
+    private Long unitPrice;           // 주문 시점 개당 가격
 
     @Column(name = "productName", nullable = false, length = 200)
     private String productName;             // 주문 시점 메뉴명
 
     @Column(name = "createdId")
-    private Long createdId;                 // 생성자 ID (BIGINT)
+    private Long createdId;                 // 생성자 ID
 
     @Column(name = "modifiedId")
-    private Long modifiedId;                // 수정자 ID (BIGINT)
+    private Long modifiedId;                // 수정자 ID
 
     @Column(name = "deletedId")
-    private Long deletedId;                 // 삭제자 ID (BIGINT)
+    private Long deletedId;                 // 삭제자 ID
 
 }
-
-// TODO: 연관관계 설정
