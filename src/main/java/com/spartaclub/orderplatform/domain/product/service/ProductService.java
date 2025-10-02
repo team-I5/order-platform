624c133244d799b7c80523f9e0c23f3697e99c3c
ooinl77 <ooinl77@naver.com>
2025-10-03 02:35:32 +0900

    feat: 상품 목록, 상세 조회 API 구현

--- src/main/java/com/spartaclub/orderplatform/domain/product/service/ProductService.java
+++ src/main/java/com/spartaclub/orderplatform/domain/product/service/ProductService.java
@@ -1,18 +1,20 @@
 package com.spartaclub.orderplatform.domain.product.service;
 
-import com.spartaclub.orderplatform.domain.product.dto.ProductResponseDto;
-import com.spartaclub.orderplatform.domain.product.dto.ProductCreateRequestDto;
-import com.spartaclub.orderplatform.domain.product.dto.ProductUpdateRequestDto;
+import com.spartaclub.orderplatform.domain.product.dto.*;
 import com.spartaclub.orderplatform.domain.product.entity.Product;
 import com.spartaclub.orderplatform.domain.product.mapper.ProductMapper;
 import com.spartaclub.orderplatform.domain.product.repository.ProductRepository;
 import com.spartaclub.orderplatform.domain.store.repository.StoreRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  * 상품 Service
@@ -22,6 +24,7 @@ import java.util.UUID;
  */
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ProductService {
 
     private final ProductRepository productRepository;
@@ -66,6 +69,7 @@ public class ProductService {
         return productMapper.toDto(product);
     }
 
+    // 상품 삭제 서비스 로직
     @Transactional
     public void deleteProduct(
             UUID productId
@@ -77,5 +81,39 @@ public class ProductService {
 
         product.deleteProduct(0L); // 도메인 메서드 호출, 회원 연결 전 하드코딩
         // @Transactional 안에서 dirty checking으로 자동 반영
+    }
+
+    // 상품 목록 조회 서비스 로직
+    public PageResponseDto<ProductResponseDto> getProductList(UUID storeId, Pageable pageable) {
+        // 1. storeId로 상품 리스트 페이지 객체로 조회
+        Page<Product> productPage = productRepository.findByStore_StoreId(storeId, pageable);
+
+        // 2. 페이지 객체에서 삼품 리스트만 추출
+        List<ProductResponseDto> productList = productPage.getContent().stream()
+                .map(productMapper::toDto)
+                .collect(Collectors.toList());
+
+        // 3. 페이지 메타 데이터 -> dto 변환
+        PageMetaDto pageMetaDto = PageMetaDto.builder()
+                .pageNumber(productPage.getNumber())
+                .pageSize(productPage.getSize())
+                .totalElements(productPage.getTotalElements())
+                .totalPages(productPage.getTotalPages())
+                .isFirst(productPage.isFirst())
+                .isLast(productPage.isLast())
+                .build();
+
+        // 4. 상품 리스트와 메타 데이터 dto 반환
+        return new PageResponseDto<>(productList, pageMetaDto);
+    }
+
+    // 상품 상세 조회 서비스 로직
+    public ProductResponseDto getProduct(UUID productId) {
+        // 1. productId로 상품 조회
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new IllegalArgumentException("상품이 존재하지 않습니다."));
+
+        // 2. Product Entity -> Dto 변환 후 반환
+        return productMapper.toDto(product);
     }
 }
