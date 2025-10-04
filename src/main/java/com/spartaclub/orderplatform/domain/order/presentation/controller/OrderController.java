5c2a949d6a22f91faaf3dbf1f040df93a2f07bfa
kmr5326 <kmr5326@naver.com>
2025-10-04 14:47:43 +0900

    fix: @Controller -> @RestController 수정

--- src/main/java/com/spartaclub/orderplatform/domain/order/presentation/controller/OrderController.java
+++ src/main/java/com/spartaclub/orderplatform/domain/order/presentation/controller/OrderController.java
@@ -12,13 +12,13 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/v1/orders")
 @RequiredArgsConstructor
 public class OrderController {
