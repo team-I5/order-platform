90c05abea627af41b6414ca29096c72b022e0a96
전우선 <jeonwooseon@naver.com>
2025-10-10 01:50:03 +0900

    feat: 주소 목록 조회 API 구현

--- src/main/java/com/spartaclub/orderplatform/user/application/mapper/AddressMapper.java
+++ src/main/java/com/spartaclub/orderplatform/user/application/mapper/AddressMapper.java
@@ -3,6 +3,8 @@ package com.spartaclub.orderplatform.user.application.mapper;
 import com.spartaclub.orderplatform.user.domain.entity.Address;
 import com.spartaclub.orderplatform.user.presentation.dto.AddressCreateRequestDto;
 import com.spartaclub.orderplatform.user.presentation.dto.AddressCreateResponseDto;
+import com.spartaclub.orderplatform.user.presentation.dto.AddressListPageResponseDto;
+import com.spartaclub.orderplatform.user.presentation.dto.AddressListResponseDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -11,7 +13,7 @@ import org.mapstruct.Mapping;
  * Address 엔티티와 DTO 간의 변환을 담당
  *
  * @author 전우선
- * @date 2025-10-09(목)
+ * @date 2025-10-10(금)
  */
 @Mapper(componentModel = "spring")
 public interface AddressMapper {
@@ -31,4 +33,38 @@ public interface AddressMapper {
     @Mapping(target = "message", constant = "주소가 성공적으로 등록되었습니다.")
     @Mapping(target = "fullAddress", expression = "java(address.getRoadNameAddress() + \" \" + address.getDetailedAddress())")
     AddressCreateResponseDto toCreateResponse(Address address);
+
+    /**
+     * Address 엔티티를 주소 목록 응답 DTO로 변환
+     * fullAddress는 별도 계산 후 설정
+     */
+    @Mapping(target = "fullAddress", expression = "java(address.getRoadNameAddress() + \" \" + address.getDetailedAddress())")
+    AddressListResponseDto toListResponse(Address address);
+
+    /**
+     * 기본 주소 정보를 DefaultAddressInfo DTO로 변환
+     */
+    default AddressListPageResponseDto.DefaultAddressInfo toDefaultAddressInfo(Address defaultAddress) {
+        if (defaultAddress == null) {
+            return null;
+        }
+        return AddressListPageResponseDto.DefaultAddressInfo.builder()
+                .addressId(defaultAddress.getAddressId())
+                .addressName(defaultAddress.getAddressName())
+                .build();
+    }
+
+    /**
+     * 주소 목록 페이지 응답 DTO를 생성
+     */
+    default AddressListPageResponseDto toPageResponse(
+            java.util.List<AddressListResponseDto> addressList,
+            long totalCount,
+            Address defaultAddress) {
+        return AddressListPageResponseDto.builder()
+                .addresses(addressList)
+                .totalCount(totalCount)
+                .defaultAddress(toDefaultAddressInfo(defaultAddress))
+                .build();
+    }
 }
