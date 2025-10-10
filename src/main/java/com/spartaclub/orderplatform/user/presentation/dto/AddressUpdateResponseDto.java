61653c36643f1f90762e21d1577d2d7d8bfd23ee
전우선 <jeonwooseon@naver.com>
2025-10-11 01:19:39 +0900

    feat: 주소 수정 API 구현

---
+++ src/main/java/com/spartaclub/orderplatform/user/presentation/dto/AddressUpdateResponseDto.java
@@ -0,0 +1,29 @@
+package com.spartaclub.orderplatform.user.presentation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * 주소 수정 응답 DTO
+ *
+ * @author 전우선
+ * @date 2025-10-11(토)
+ */
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddressUpdateResponseDto {
+
+    private String message;
+    private UUID addressId;
+    private String addressName;
+    private String fullAddress;
+    private Boolean defaultAddress;
+    private LocalDateTime modifiedAt;
+}
