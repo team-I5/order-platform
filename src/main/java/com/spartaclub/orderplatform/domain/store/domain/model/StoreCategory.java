3d27850b0edb81788c81b73edb382fdcdc17ff92
Hyunju Lee <guswnee00@gmail.com>
2025-10-11 13:06:48 +0900

    feat: 음식점 카테고리 중간 테이블 생성

---
+++ src/main/java/com/spartaclub/orderplatform/domain/store/domain/model/StoreCategory.java
@@ -0,0 +1,58 @@
+package com.spartaclub.orderplatform.domain.store.domain.model;
+
+import com.spartaclub.orderplatform.domain.category.entity.Category;
+import com.spartaclub.orderplatform.global.domain.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
+@Entity
+@Table(name = "p_stores_categories")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class StoreCategory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID storeCategoryId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private Long createdId;
+
+    @LastModifiedBy
+    private Long modifiedId;
+
+    private Long deletedId;
+
+    public StoreCategory(Store store, Category category) {
+        this.store = store;
+        this.category = category;
+    }
+
+    public void scSoftDelete(Long userId) {
+        this.deletedId = userId;
+    }
+}
