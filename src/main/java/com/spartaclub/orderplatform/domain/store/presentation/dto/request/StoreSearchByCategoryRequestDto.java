1c6542349c52eb0eb82229a658bc4d1a57666c3d
Hyunju Lee <guswnee00@gmail.com>
2025-10-11 13:07:40 +0900

    feat: 음식점 카테고리별 목록 조회 dto

---
+++ src/main/java/com/spartaclub/orderplatform/domain/store/presentation/dto/request/StoreSearchByCategoryRequestDto.java
@@ -0,0 +1,25 @@
+package com.spartaclub.orderplatform.domain.store.presentation.dto.request;
+
+import com.spartaclub.orderplatform.domain.category.entity.CategoryType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class StoreSearchByCategoryRequestDto {
+
+    private CategoryType categoryType;
+    private int page = 0;
+    private int size = 10;
+    private Sort.Direction sortDirection = Direction.DESC;
+
+    public void validatePageSize() {
+        if (size != 10 && size != 30 && size != 50) {
+            this.size = 10;
+        }
+    }
+}
