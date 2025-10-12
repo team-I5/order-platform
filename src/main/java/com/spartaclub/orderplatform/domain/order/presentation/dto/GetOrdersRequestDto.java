abd260770816bfbd5c78cf4260c40995edcdccb2
kmr5326 <kmr5326@naver.com>
2025-10-13 00:27:43 +0900

    feat: 주문 목록 조회 Specification 적용, 주문 상태별 필터링 + Role에 따른 조회 제한

--- src/main/java/com/spartaclub/orderplatform/domain/order/presentation/dto/GetOrdersRequestDto.java
+++ src/main/java/com/spartaclub/orderplatform/domain/order/presentation/dto/GetOrdersRequestDto.java
@@ -1,5 +1,7 @@
 package com.spartaclub.orderplatform.domain.order.presentation.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.spartaclub.orderplatform.domain.order.domain.model.OrderStatus;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import java.util.List;
@@ -12,7 +14,10 @@ public record GetOrdersRequestDto(
     @Max(value = 100, message = "최대 100개까지만 조회할 수 있습니다.")
     Integer size,
 
-    List<String> sort
+    List<String> sort,
+
+    @JsonAlias({"statuses", "status"})
+    List<OrderStatus> status
 ) {
 
     //기본값 지정
