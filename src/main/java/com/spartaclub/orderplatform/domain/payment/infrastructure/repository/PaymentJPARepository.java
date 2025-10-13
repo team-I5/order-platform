bef9c63d84d9f3ea19839f64f19636dc12c57056
kmr5326 <kmr5326@naver.com>
2025-10-13 17:48:25 +0900

    refactor: Payment repository DIP 구조로 리팩토링

--- src/main/java/com/spartaclub/orderplatform/domain/payment/infrastructure/repository/PaymentJPARepository.java
+++ src/main/java/com/spartaclub/orderplatform/domain/payment/infrastructure/repository/PaymentJPARepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PaymentRepository extends JpaRepository<Payment, UUID>,
+public interface PaymentJPARepository extends JpaRepository<Payment, UUID>,
     JpaSpecificationExecutor<Payment> {
 
     boolean existsByOrder(Order order);
