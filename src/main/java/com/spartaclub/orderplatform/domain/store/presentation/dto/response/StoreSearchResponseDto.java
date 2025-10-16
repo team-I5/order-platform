71b9a5b6b1d52c5c50da1c86f48909d1368983e9
guswnee00 <guswnee00@gmail.com>
2025-10-16 22:21:49 +0900

    refactor: response 통합

--- src/main/java/com/spartaclub/orderplatform/domain/store/presentation/dto/response/StoreSearchResponseDto.java
+++ src/main/java/com/spartaclub/orderplatform/domain/store/presentation/dto/response/StoreSearchResponseDto.java
@@ -1,12 +1,17 @@
 package com.spartaclub.orderplatform.domain.store.presentation.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class StoreSearchResponseDto {
+
     private String storeName;
     private Double averageRating;
     private Integer reviewCount;
+    private List<String> categories;
 }
